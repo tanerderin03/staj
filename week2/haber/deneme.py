759040510b8122fe48877876bc1320a5562ee3fa
tanerderin03 <tanerderin03@gmail.com>
2025-08-17 16:54:47 +0300

    streamlit ui added

--- week2/haber/deneme.py
+++ week2/haber/deneme.py
@@ -1,22 +1,25 @@
 import os
 from dotenv import load_dotenv
 import google.generativeai as genai 
-
-
+import streamlit as st
 load_dotenv()
-
-
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-
 model=genai.GenerativeModel("gemini-1.5-flash")
+st.title("Haber özeti")
+st.write("Haberi yaz")
+def get_response(txt,mdl):
+    response=mdl.generate_content("haberi özetle max3 cümle: "+txt)
+    return response.text
 
 
-with open("haber.txt") as f:
-    text=f.read()
+with st.form("form1"):
+    text = st.text_area(" ")
+    submitted = st.form_submit_button("Gönder") 
 
 
+if submitted:
+    if not text.strip(): 
+        st.error("Lütfen önce bir haber girin!")
+    else:
+        st.success(get_response(text, model))
 
-response=model.generate_content("haberi özetle max5 cümle: "+text)
-
-
-print(response.text)
